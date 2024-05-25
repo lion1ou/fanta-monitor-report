a03c2a672822d1bf7db4bb52436c353e5bae29ac
lion1ou <lion1ou@163.com>
2024-05-26 00:19:36 +0800

    fix: 修改打包文件

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,82 +1,81 @@
 // rollup 配置文件
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import rollupTypescript from "rollup-plugin-typescript2";
-import babel from "@rollup/plugin-babel";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import rollupTypescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
-import terser from "@rollup/plugin-terser"; 
+import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
-import { DEFAULT_EXTENSIONS } from "@babel/core";
-import pkg from "./package.json" assert { type: 'json' }; // 读取 package.json 配置
-import replace from "@rollup/plugin-replace";
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import pkg from './package.json' assert { type: 'json' }; // 读取 package.json 配置
+import replace from '@rollup/plugin-replace';
 const env = process.env.NODE_ENV; // 当前运行环境，可通过 cross-env 命令行设置
-const name = "FantaReport"; // 导出的全局变量名称
+const name = 'FantaReport'; // 导出的全局变量名称
 const config = {
-  input: "src/main.ts",
+  input: 'src/main.ts',
   output: [
     {
-      file: pkg.main,
-      format: "cjs",
+      file: `dist/fanta-report-${pkg.version}-cjs.js`,
+      format: 'cjs',
     },
     {
-      file: pkg.module,
-      format: "es",
+      file: `dist/fanta-report-${pkg.version}-es.js`,
+      format: 'es',
     },
     {
-      file: pkg.umd,
-      format: "umd",
+      file: `dist/fanta-report-${pkg.version}-umd.js`,
+      format: 'umd',
       name,
     },
   ],
   plugins: [
     json(),
-     // 解析第三方依赖 
-    resolve({ jsnext: true, preferBuiltins: true, browser: true }), 
-     // 识别 commonjs 模式第三方依赖 
-     commonjs(),
-     // rollup 编译 typescript 
-    rollupTypescript(), 
-     // babel 配置 
-    babel({ 
-        babelHelpers: 'runtime',
-        skipPreflightCheck: true,
-         // 只转换源代码，不转换外部依赖 
-         exclude: 'node_modules/**', 
-         // babel 默认不支持 ts 需要手动添加 
-         extensions: [ 
-             ...DEFAULT_EXTENSIONS, 
-             '.ts', 
-         ], 
+    // 解析第三方依赖
+    resolve({ jsnext: true, preferBuiltins: true, browser: true }),
+    // 识别 commonjs 模式第三方依赖
+    commonjs(),
+    // rollup 编译 typescript
+    rollupTypescript(),
+    // babel 配置
+    babel({
+      babelHelpers: 'runtime',
+      skipPreflightCheck: true,
+      // 只转换源代码，不转换外部依赖
+      exclude: 'node_modules/**',
+      // babel 默认不支持 ts 需要手动添加
+      extensions: [...DEFAULT_EXTENSIONS, '.ts'],
     }),
     replace({
-      '__VERSION__': pkg.version,
-      '__BUILDTIME__': new Date().toLocaleString(),
-      '__ENV__': env
-    })
+      __VERSION__: pkg.version,
+      __BUILDTIME__: new Date().toLocaleString(),
+      __ENV__: env,
+    }),
   ],
 };
 
-// 若打包正式环境，压缩代码 
-if (env === 'pro') { 
-  config.plugins.push(terser({ 
-      compress: { 
-          pure_getters: true, 
-          unsafe: true, 
-          unsafe_comps: true, 
-          warnings: false 
-      } 
-  })) 
-} 
+// 若打包正式环境，压缩代码
+if (env === 'pro') {
+  config.plugins.push(
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    })
+  );
+}
 
 // 测试环境才需要服务
-if (env !== 'pro') { 
+if (env !== 'pro') {
   config.plugins.push(
     serve({
       contentBase: ['example', 'dist'],
       port: 3388,
       open: true,
     })
-  ) 
-} 
+  );
+}
 
 export default config;
